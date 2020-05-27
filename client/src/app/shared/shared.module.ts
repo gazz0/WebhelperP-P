89aee32b2b022c702acd21e725f4ffc9d9048737
gazz0 <49281567+gazz0@users.noreply.github.com>
2020-05-27 11:46:56 +0200

    componenten versuch

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -7,6 +7,9 @@ import { AngularMaterialModule } from '../angular-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { UserMenuComponent } from './components/user-menu/user-menu.component';
 import { Header2Component } from './components/header2/header2.component';
+import { SidebarRightComponent } from './components/sidebar-right/sidebar-right.component';
+import { SidebarLeftComponent } from './components/sidebar-left/sidebar-left.component';
+import { SearchResultElementComponent } from './components/search-result-element/search-result-element.component';
 
 
 
@@ -14,7 +17,7 @@ import { Header2Component } from './components/header2/header2.component';
   declarations: [
     HeaderComponent, 
     FooterComponent, 
-    SidebarComponent, UserMenuComponent, Header2Component],
+    SidebarComponent, UserMenuComponent, Header2Component, SidebarRightComponent, SidebarLeftComponent, SearchResultElementComponent],
   imports: [
     CommonModule,
     AngularMaterialModule,
@@ -25,6 +28,9 @@ import { Header2Component } from './components/header2/header2.component';
     Header2Component,
     FooterComponent,
     SidebarComponent,
+    SidebarRightComponent,
+    SidebarLeftComponent,
+    SearchResultElementComponent
   ]
 })
 export class SharedModule { }
